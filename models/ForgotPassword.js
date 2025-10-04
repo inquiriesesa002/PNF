c33b132e20b6da3cba015dc3879d7eb4c9bd3403
inquiriesesa002 <inquiriesesa@gmail.com>
2025-10-04 16:48:03 +0500

    Delete models directory

--- models/ForgotPassword.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const forgotPasswordSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  resetToken: { type: String, required: true },
-  resetTokenExpiry: { type: Date, required: true },
-});
-
-module.exports = mongoose.model("ForgotPassword", forgotPasswordSchema);
